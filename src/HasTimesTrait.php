b87da082d89cebc4c67da1bdd054979d2618a5ad
Brian Faust <faustbrian@users.noreply.github.com>
2016-11-23 14:20:48 +0200

    Applied fixes from StyleCI (#2)
    [ci skip] [skip ci]

--- src/HasTimesTrait.php
+++ src/HasTimesTrait.php
@@ -1,9 +1,17 @@
 <?php
 
+/*
+ * This file is part of Laravel Timeable.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Timeable;
 
 use Carbon\Carbon;
-use BrianFaust\Timeable\Time;
 
 trait HasTimesTrait
 {
