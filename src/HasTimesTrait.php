aa50252040195b255b685ae8c41fafb0d48ed9fd
Brian Faust <hello@brianfaust.de>
2016-12-04 17:26:41 +0200

    More php 7.1 return types

--- src/HasTimesTrait.php
+++ src/HasTimesTrait.php
@@ -11,27 +11,19 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Laravel Timeable.
- *
- * (c) Brian Faust <hello@brianfaust.de>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace BrianFaust\Timeable;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 trait HasTimesTrait
 {
-    public function times()
+    public function times(): MorphMany
     {
         return $this->morphMany(Time::class, 'timeable');
     }
 
-    public function addTime($day, $open, $close)
+    public function addTime($day, $open, $close): bool
     {
         $time = new Time([
             'day'   => $day,
@@ -39,6 +31,6 @@ trait HasTimesTrait
             'close' => new Carbon($close),
         ]);
 
-        $this->times()->save($time);
+        return (bool) $this->times()->save($time);
     }
 }
--- src/Time.php
+++ src/Time.php
@@ -11,24 +11,16 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of Laravel Timeable.
- *
- * (c) Brian Faust <hello@brianfaust.de>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace BrianFaust\Timeable;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Time extends Model
 {
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
-    public function timeable()
+    public function timeable(): MorphTo
     {
         return $this->morphTo();
     }
