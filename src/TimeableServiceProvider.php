ea4d6be1d52243d17437578f6b7042119ba8cd91
Brian Faust <hello@brianfaust.de>
2017-06-06 11:04:25 +0300

    some comments

--- src/TimeableServiceProvider.php
+++ src/TimeableServiceProvider.php
@@ -15,6 +15,9 @@ use Illuminate\Support\ServiceProvider;
 
 class TimeableServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap any application services.
+     */
     public function boot()
     {
         $this->publishes([
--- src/Traits/HasTimesTrait.php
+++ src/Traits/HasTimesTrait.php
@@ -25,8 +25,8 @@ trait HasTimesTrait
     public function addTime($day, $open, $close): bool
     {
         $time = new Time([
-            'day'   => $day,
-            'open'  => new Carbon($open),
+            'day' => $day,
+            'open' => new Carbon($open),
             'close' => new Carbon($close),
         ]);
 
